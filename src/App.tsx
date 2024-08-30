fed046b6f98905f223a8996ad5cb25bff33d7840
mikyven <alexpry09@gmail.com>
2024-08-30 14:28:26 +0300

    added hours; added time in the title; fixed notification without image problem

--- src/App.tsx
+++ src/App.tsx
@@ -19,18 +19,24 @@ function detectMobile(): boolean {
 }
 
 function App(): ReactElement {
-  const [time, setTime] = useState(0);
+  const [time, setTime] = useState(Number(localStorage.getItem('initialTime')) || 0);
   const [isStarted, setIsStarted] = useState(false);
   const isMobile = detectMobile();
-  const minutes = Math.floor(time / 60);
-  const seconds = time - minutes * 60;
+  const hours = Math.floor(time / (60 * 60));
+  const minutes = Math.floor((time - hours * 3600) / 60);
+  const seconds = time - hours * 3600 - minutes * 60;
 
   useEffect(() => {
     Notification.requestPermission();
   }, []);
 
+  useEffect(() => {
+    localStorage.setItem('initialTime', `${time}`);
+  }, [time]);
+
   const resetTime = (): void => {
     setIsStarted(false);
+    document.title = 'Timer';
     setTime(0);
   };
 
@@ -47,25 +53,54 @@ function App(): ReactElement {
 
   const handleWheel = (e: WheelEvent, type: string): void => {
     if (!isStarted) {
+      const maxHoursCondition = type === 'hours' && hours === 99;
       const maxMinutesCondition = type === 'minutes' && minutes === 59;
-      const maxTimeCondition = type === 'seconds' && time === 60 * 60 - 1;
-      if (e.deltaY > 0 && time === 0) return;
-      if (e.deltaY < 0 && (maxMinutesCondition || maxTimeCondition)) return;
-      const value = type === 'seconds' ? 1 : 60;
+      const maxTimeCondition = time === 99 * 60 * 60 - 1;
+      if (e.deltaY > 0 && (time === 0 || (type === 'minutes' && minutes === 0) || (type === 'hours' && hours === 0))) {
+        return;
+      }
+      if (e.deltaY < 0 && (maxHoursCondition || maxMinutesCondition || maxTimeCondition)) {
+        return;
+      }
+      let value = 0;
+      switch (type) {
+        case 'hours':
+          value = 3600;
+          break;
+        case 'minutes':
+          value = 60;
+          break;
+        case 'seconds':
+          value = 1;
+          break;
+        default:
+          break;
+      }
       setTime(e.deltaY < 0 ? time + value : time - value);
     }
   };
+
+  function parseTime(t: number): string {
+    const convertTime = (value: number): string => `${value < 10 ? `0${value}` : value}`;
+    const parsedHours = Math.floor(t / (60 * 60));
+    const parsedMinutes = Math.floor((t - parsedHours * 3600) / 60);
+    const parsedSeconds = t - parsedHours * 3600 - parsedMinutes * 60;
+
+    return `${convertTime(parsedHours)}:${convertTime(parsedMinutes)}:${convertTime(parsedSeconds)}`;
+  }
 
   /* eslint-disable prettier/prettier */
   const tick = useCallback(() => {
     if (isStarted) {
       if (time === 1) {
         resetTime();
-        const img = '/public/clock.png';
+        const img = '/assets/clock.png';
         const text = 'Your timer has ended.';
         // eslint-disable-next-line no-new
         new Notification('Timer', { body: text, icon: img });
+        return;
       }
+      document.title = `${parseTime(time - 1)} | Timer`;
       setTime(time - 1);
     }
   }, [isStarted, time]
@@ -75,6 +110,7 @@ function App(): ReactElement {
   const startTimer = (): void => {
     if (time === 0) return;
     setIsStarted(true);
+    document.title = `${parseTime(time)} | Timer`;
   };
   const stopTimer = (): void => setIsStarted(false);
 
@@ -86,12 +122,19 @@ function App(): ReactElement {
       {!isMobile && (
         <div className="timer">
           <div className="time">
+            <div className="hours time-element" onWheel={(e) => handleWheel(e, 'hours')}>
+              <p style={{ width: hours > 9 ? '125px' : '65px' }}>{hours}</p>
+              <span className="time-unit">h</span>
+            </div>
             <div className="minutes time-element" onWheel={(e) => handleWheel(e, 'minutes')}>
-              <p style={{ width: minutes < 10 ? '65px' : '125px' }}>{minutes}</p>
+              <p>
+                {minutes < 10 && '0'}
+                {minutes}
+              </p>
               <span className="time-unit">m</span>
             </div>
             <div className="seconds time-element" onWheel={(e) => handleWheel(e, 'seconds')}>
-              <p style={{ width: '125px' }}>
+              <p>
                 {seconds < 10 && '0'}
                 {seconds}
               </p>
